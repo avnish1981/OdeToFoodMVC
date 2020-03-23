bdce4fa78f041036a9ba561ec1dcc6d3f02957ab
avnish1981 <avnish.choubey@gmail.com>
2020-03-23 13:31:45 +0530

    Adding new files

---
+++ OdeToFood.Data/Services/clsInMemoryData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using OdeToFood.Data.Models;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class clsInMemoryData : IRestaurantData
+    {
+        List<Restaurant> objRes = null;
+        public clsInMemoryData()
+        {
+            objRes = new List<Restaurant>
+            {
+               new Restaurant {  Id = 1, Name = "Avnish Restaurant", Location = "London", Cuisine = CuisineType.Indian },
+               new Restaurant { Id = 2, Name = "Arti Restaurant", Location = "Delhi", Cuisine = CuisineType.Mexican },
+               new Restaurant { Id = 3, Name = "Twinkle Restaurant", Location = "Ranchi", Cuisine = CuisineType.Indian },
+               new Restaurant { Id = 4, Name = "Vedaant Restaurant", Location = "Ranchi", Cuisine = CuisineType.Mexican  }
+
+             };
+        }
+             
+        public IEnumerable <Restaurant> GetAllRestaurant()
+        {
+            return objRes.OrderBy(r => r.Name);
+        }
+    }
+}
--- OdeToFoodMVC.Web/Controllers/HomeController.cs
+++ OdeToFoodMVC.Web/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿
+using OdeToFood.Data.Models;
+using OdeToFood.Data.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,17 @@ namespace OdeToFoodMVC.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+
+        IRestaurantData db = null;
+        
+        public HomeController()
         {
-            return View();
+            db = new clsInMemoryData();
+        }
+        public ActionResult  Index()
+        {
+            var model = db.GetAllRestaurant();
+            return View(model);
         }
 
         public ActionResult About()
