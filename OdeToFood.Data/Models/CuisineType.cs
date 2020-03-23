c2a12b388ea4fa79506c6254ba7b86cc9498268c
avnish1981 <avnish.choubey@gmail.com>
2020-03-23 11:03:33 +0530

    Initial Commit

---
+++ OdeToFood.Data/Models/CuisineType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data.Models
+{
+    public enum CuisineType
+    {
+        None,
+        Indian,
+        Mexican,
+        Italian
+    }
+}
