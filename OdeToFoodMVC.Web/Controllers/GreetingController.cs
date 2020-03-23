ce3459996874238ffca5abcfd5f37a8746848b28
avnish1981 <avnish.choubey@gmail.com>
2020-03-23 15:54:04 +0530

    Adding files

---
+++ OdeToFoodMVC.Web/Controllers/GreetingController.cs
@@ -0,0 +1,23 @@
+﻿using OdeToFoodMVC.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFoodMVC.Web.Controllers
+{
+    public class GreetingController : Controller
+    {
+        
+        // GET: Greeting
+        public ActionResult Index()
+        {
+            var model = new GreetingModel();
+            model.proMessage = ConfigurationManager.AppSettings["disMessage"];
+                       
+            return View(model);
+        }
+    }
+}
